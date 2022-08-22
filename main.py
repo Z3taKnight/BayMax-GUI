bd5060e3c9f32fef7a999e80e20bbdb46fe73820
Ayush Srivastav <ayushsrivastav11@gmail.com>
2022-08-22 11:53:52 +0530

    test

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
+from json import load
 import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import QTimer, QTime, QDate, QtMsgType
 from PyQt5.QtGui import QMovie
 from PyQt5.QtCore import *
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.uic import loadUiType
+from PyQt5.QtWidgets import QSplashScreen
+from PyQt5.uic import loadUiType, loadUi
 from frontend import Ui_MainWindow
+from loading import Ui_loading
+from binascii import rlecode_hqx
+from cgitb import text
+from email import message
+from email.mime import audio
+from fileinput import filename
+import os
+from time import sleep
+from turtle import done
+from unittest import skip
+from neuralintents import GenericAssistant
+from pyparsing import null_debug_action
+import speech_recognition as sr
+import pyttsx3 as tts
+import sys
+import pywhatkit
+import webbrowser
+from gtts import gTTS
+import playsound
+import re
+import random
+import pyjokes
+import wikipedia
+from socket import timeout
+import time
+import schedule
+from matplotlib.pyplot import title
+from plyer import notification
 import requests
 
 class MainThread(QThread):
@@ -25,13 +56,13 @@ class Main(QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.close)
 
     def startTask(self):
-        self.ui.movie =  QtGui.QMovie("tumblr_nfetk2UwcB1sj5h4oo1_1280.webp")  
+        self.ui.movie =  QtGui.QMovie("Assests/Normal_Screen.webp")  
         self.ui.label.setMovie(self.ui.movie)
         self.ui.movie.start()
         timer = QTimer(self)
         timer.timeout.connect(self.showTime)
         timer.start(1000)
-        startExecution.start()
+        
 
     def showTime(self):
         current_time=QTime.currentTime()
@@ -42,12 +73,19 @@ class Main(QMainWindow):
         self.ui.textBrowser_2.setText(label_time)
 
     def steps(self):
-        url = "https://v1.nocodeapi.com/zetaknight/fit/GpWLJGwidrAOkBVc/aggregatesDatasets?dataTypeName=steps_count&timePeriod=today"
+        url = "https://v1.nocodeapi.com/zetaknight/fit/GpWLJGwidrAOkBVc/aggregatesDatasets?dataTypeName=steps_count,active_minutes,calories_expended&timePeriod=today"
         params = {}
-        steps = requests.get(url = url, params = params)
-        steps_c = steps.json()
-        st=str(steps_c['steps_count'][0]['value'])
-        self.ui.textBrowser_3.setText(st)
+        r = requests.get(url = url, params = params)
+        result = r.json()
+        steps=str(result['steps_count'][0]['value'])
+        active=str(result['active_minutes'][0]['value'])
+        calories=str(result['calories_expended'][0]['value'])
+        self.ui.textBrowser_3.setText(steps)
+        self.ui.textBrowser_7.setText(active)
+        self.ui.textBrowser_8.setText(calories)
+
+        
+
 
 
 app = QApplication(sys.argv)
